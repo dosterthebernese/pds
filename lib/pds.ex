4bed4c7f2d41ae98854e50088f177c5822ed70ca
neil <orderllrobbieesquire@gmail.com>
2022-07-13 18:19:39 -0400

    first commit

---
+++ lib/pds.ex
@@ -0,0 +1,18 @@
+defmodule PDS do
+  @moduledoc """
+  Documentation for `PDS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PDS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pds_test.exs
@@ -0,0 +1,8 @@
+defmodule PDSTest do
+  use ExUnit.Case
+  doctest PDS
+
+  test "greets the world" do
+    assert PDS.hello() == :world
+  end
+end
